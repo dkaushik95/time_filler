5689d5879f9adce4b33f3528968474eae4bba58c
Dishant Kaushik <dkaushik95@gmail.com>
2019-03-14 17:31:36 -0400

    Updated time logic

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-const moment = require('moment');
+const moment = require('moment')
 
-// randomly tell you what times to put in
-// hours:minutes to hours:minutes
-// 2 hours 5 times
-// 10am - 6pm
-// friday - thursday
-// 0 6 = dayofWeel
-// time 10 - 19 hrs
-// min 0 - 60 mins
+
 const subtractor = [2, 3, 4, 5, 6, 0, 1]
-const year = moment().format("YYYY")
-const month = moment().format("MM")
+const mom = moment()
+const year = mom.format('YYYY')
+const month = mom.format('MM')
 const friday = moment().subtract(subtractor[moment().format('e')], 'day')
 
 const getRandom = (min, max) => {
@@ -23,40 +17,83 @@ const getRandom = (min, max) => {
     return ans;
 }
 
-let hours = []
-let from = {
+let gaHours = []
+let taHours = []
+
+let gafrom = {
     day: parseInt(friday.format('D') - 1),
     hr: getRandom(10, 19),
     mm: getRandom(0, 60)
 }
-for(let i = 0; i < 5; i++){
-    from = {
-        day: parseInt(from.day) + 1,
+
+let tafrom = {
+    day: parseInt(friday.format('D') - 1),
+    hr: getRandom(10, 19),
+    mm: getRandom(0, 60)
+}
+
+for(let i = 0;i < 5; i++){
+    gafrom = {
+        day: parseInt(gafrom.day) + 1,
         hr: getRandom(10, 19),
         mm: getRandom(0, 60)
     }
-    const forma = `${year}-${month}-${from.day} ${from.hr}:${from.mm}`
-    let fromTime = moment(forma)
-    let toTime = moment(forma)
-    toTime.add(2, 'hour')
+
+    const gaForma = `${year}-${month}-${gafrom.day} ${gafrom.hr}:${gafrom.mm}`
+    let gaFromTime = moment(gaForma)
+    let gaToTime = moment(gaForma)
+    gaToTime.add(2, 'hour')
     
-    hours.push({
-        date: fromTime.format('MM/DD/YYYY'),
-        from: fromTime.format('hh:mm'),
-        to: toTime.format('hh:mm')
+    gaHours.push({
+        date: gaFromTime.format('MM/DD/YYYY'),
+        from: gaFromTime.format('hh:mm'),
+        to: gaToTime.format('hh:mm')
     })
+
+    gaToTime.add(2, 'hour')
+
+    tafrom = {
+        day: parseInt(tafrom.day) + 1,
+        hr: parseInt(gaToTime.format('hh')),
+        mm: getRandom(0, 60)
+    }
+
+    const taforma = `${year}-${month}-${tafrom.day} ${tafrom.hr}:${tafrom.mm}`
+    let tafromtime = moment(taforma)
+    let tatotime = moment(taforma)
+    tatotime.add(2, 'hour')
+    
+    taHours.push({
+        date: tafromtime.format('MM/DD/YYYY'),
+        from: tafromtime.format('hh:mm'),
+        to: tatotime.format('hh:mm')
+    })
+
 }
+
 console.log(`
 Hi Bridgette,
 
-Here is my timecard for this week.
-
+Here is my timecard for the week:
 TA(7044701):
+
 `)
-hours.map(h => {
+
+taHours.map(h => {
     console.log('\n');
     console.log(`Date: ${h.date}`)
     console.log(`In: ${h.from}`)
     console.log(`Out: ${h.to}`)
 })
 
+console.log('\n GA(7044771')
+
+gaHours.map(h => {
+    console.log('\n');
+    console.log(`Date: ${h.date}`)
+    console.log(`In: ${h.from}`)
+    console.log(`Out: ${h.to}`)
+})
+
+
+console.log('\n Thanks, \n Dishant')
